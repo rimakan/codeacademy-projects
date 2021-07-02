5c15f9951bbd1ce68004f9a4e2bf484bf23fca8f
Kirill Fedorenko <fedorenko.kiril@gmail.com>
2021-07-02 10:30:48 +0300

    test/added new test

--- cypress/integration/reset_pass_page_spec.js
+++ cypress/integration/reset_pass_page_spec.js
@@ -1,5 +1,6 @@
 const url = 'https://www.codeacademy.com/login';
 const email = 'netehih761@advew.com';
+const wrongEmail = 'testtest.com'
 
 describe('user wants to restore the password', () => {
     it('redirects to the restore page', () => {
@@ -16,6 +17,16 @@ describe('user sends a request to reset the password', () => {
         cy.contains('Please enter your email:').type(email)
         cy.get('.btn-brand-purple__3JIxHxx4KYF2OVbx8Jg6mb').click()
         cy.contains('Password Reset Sent').should('be.visible')
+    });
+})
+
+describe('reset a password using incorrect email', () => {
+    it('', () => {
+        cy.visit(url)
+        cy.contains('I forgot').click()
+        cy.contains('Please enter your email:').type(wrongEmail)
+        cy.get('.btn-brand-purple__3JIxHxx4KYF2OVbx8Jg6mb').click()
+        cy.contains('Enter a valid email').should('be.visible')
     });
 })
 
